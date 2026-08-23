d443bd4e72f202aa28b8315878cd73a7712052f1
Utkarsh2006-Abhaipur <138103800+Utkarsh2006-Abhaipur@users.noreply.github.com>
2026-08-23 23:10:46 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,4 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
 
 import {
     getAuth,
@@ -8,6 +10,10 @@ import {
     signOut,
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+
+import {
+    getFirestore
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-firestore.js";
 
 
 const firebaseConfig = {
@@ -25,12 +31,15 @@ const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
+const db = getFirestore(app);
+
 const googleProvider = new GoogleAuthProvider();
 
 
 export {
     app,
     auth,
+    db,
     googleProvider,
     signInWithRedirect,
     getRedirectResult,
