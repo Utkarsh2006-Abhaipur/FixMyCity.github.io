1a322e16ecf909ddf4c0dcbd3907a1b2eb260784
Utkarsh2006-Abhaipur <138103800+Utkarsh2006-Abhaipur@users.noreply.github.com>
2026-08-23 21:48:28 +0530

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -9,20 +9,24 @@ import {
     onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
 
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDU1F5eJplYnQ7HrIBcxvK3jA3ZMECyq2w",
-    authDomain: "civicissuereporter-2fa6d.firebaseapp.com",
-    projectId: "civicissuereporter-2fa6d",
-    storageBucket: "civicissuereporter-2fa6d.firebasestorage.app",
-    messagingSenderId: "1008677836126",
-    appId: "1:1008677836126:web:1b1660f450d8d6657aabcf"
+    apiKey: "AIzaSyAMVMAwc3k-MEFnnEOLvxyJfEnesWVrC-w",
+    authDomain: "citycare-c2642.firebaseapp.com",
+    projectId: "citycare-c2642",
+    storageBucket: "citycare-c2642.firebasestorage.app",
+    messagingSenderId: "703326046335",
+    appId: "1:703326046335:web:7eb39da54ab6f7af8c574a",
+    measurementId: "G-NTMHFH6D9C"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
 
 const googleProvider = new GoogleAuthProvider();
+
 
 export {
     app,
