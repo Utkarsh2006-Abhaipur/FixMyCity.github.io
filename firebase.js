2e11460b24ac7655e540d2b4d3ae0aa28b52bb22
Utkarsh2006-Abhaipur <138103800+Utkarsh2006-Abhaipur@users.noreply.github.com>
2026-08-23 16:54:56 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyB-T9XRVZ0pIFgbiTeItmmyepaCm7hVGtA",
+    authDomain: "civicreporter-641b9.firebaseapp.com",
+    projectId: "civicreporter-641b9",
+    storageBucket: "civicreporter-641b9.firebasestorage.app",
+    messagingSenderId: "843837358275",
+    appId: "1:843837358275:web:ee38aa651e82e2525d6276"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
